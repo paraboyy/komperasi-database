dc014e8a606b6d0377bd358fc5e4a462bf6883b2
adhim <adhimxtkj@gmail.com>
2024-05-27 13:01:07 +0800

    update: Code DB JSON --relasional

--- db_json.py
+++ db_json.py
@@ -20,7 +20,7 @@ def compare_insert_operation():
             INSERT INTO products_json (product_data) VALUES ('{"name": "New Product", "price": 99, "category": "Test Category", "description": "Test Description"}')
             """
             start_time = time.time() # Waktu awal komparasi
-            for i in range(1000):  # Misalnya, melakukan operasi insert sebanyak 1000 kali
+            for i in range(1000):  # Melakukan operasi insert sebanyak 1000 kali
                 cursor.execute(query)
             connection.commit()
             end_time = time.time() # Waktu akhir komparasi
@@ -29,7 +29,7 @@ def compare_insert_operation():
             print("Insert operation execution time: {} seconds".format(execution_time))
 
             # Simpan waktu komparasi ke dalam file
-            with open('results.txt', 'a') as file:
+            with open('hasil/results.txt', 'a') as file:
                 file.write("Insert operation execution time: {} seconds\n".format(execution_time))
 
             cursor.close()
@@ -56,7 +56,8 @@ def compare_update_operation():
             UPDATE products_json SET product_data = '{"name": "Updated Product", "price": 199, "category": "Updated Category", "description": "Updated Description"}' WHERE JSON_EXTRACT(product_data, '$.name') = 'New Product'
             """
             start_time = time.time() # Waktu awal komparasi
-            cursor.execute(query)
+            for i in range(1000):  # Melakukan operasi update sebanyak 1000 kali
+                cursor.execute(query)
             connection.commit()
             end_time = time.time() # Waktu akhir komparasi
             execution_time = end_time - start_time
@@ -64,7 +65,7 @@ def compare_update_operation():
             print("Update operation execution time: {} seconds".format(execution_time))
 
             # Simpan waktu komparasi ke dalam file
-            with open('results.txt', 'a') as file:
+            with open('hasil/results.txt', 'a') as file:
                 file.write("Update operation execution time: {} seconds\n".format(execution_time))
 
             cursor.close()
@@ -91,7 +92,8 @@ def compare_delete_operation():
             DELETE FROM products_json WHERE JSON_EXTRACT(product_data, '$.name') = 'Updated Product'
             """
             start_time = time.time() # Waktu awal komparasi
-            cursor.execute(query)
+            for i in range(1000):  # Melakukan operasi delete sebanyak 1000 kali
+                cursor.execute(query)
             connection.commit()
             end_time = time.time() # Waktu akhir komparasi
             execution_time = end_time - start_time
@@ -99,7 +101,7 @@ def compare_delete_operation():
             print("Delete operation execution time: {} seconds".format(execution_time))
 
             # Simpan waktu komparasi ke dalam file
-            with open('results.txt', 'a') as file:
+            with open('hasil/results.txt', 'a') as file:
                 file.write("Delete operation execution time: {} seconds\n".format(execution_time))
 
             cursor.close()
@@ -108,8 +110,8 @@ def compare_delete_operation():
     except Error as e:
         print("Error:", e)
 
-with open('results.txt', 'a') as file:
-                file.write("Komparasi DB JSON tanpa Caching\n")
+with open('hasil/results.txt', 'a') as file:
+    file.write("Komparasi DB JSON tanpa Caching\n")
 
 # Panggil fungsi untuk melakukan komparasi operasi insert
 compare_insert_operation()
@@ -120,5 +122,5 @@ compare_update_operation()
 # Panggil fungsi untuk melakukan komparasi operasi delete
 compare_delete_operation()
 
-with open('results.txt', 'a') as file:
-                file.write("----------------------------------------\n")
+with open('hasil/results.txt', 'a') as file:
+    file.write("----------------------------------------\n")
